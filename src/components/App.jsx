68b518d212e917bd5886aea604600710ef1435a8
Berlin Sky <pedroyoung8989@gmail.com>
2016-04-28 12:57:49 -0700

    Implement Flux/Alt

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,29 @@ import React from 'react';
 import MessageList from './MessageList.jsx';
 import ChannelList from './ChannelList.jsx';
 import MessageBox from './MessageBox.jsx';
+import Login from './Login.jsx';
 
 import AppBar from 'material-ui/lib/app-bar';
 import RaisedButton from 'material-ui/lib/raised-button';
-
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
-
 import MyRawTheme from '../app-mui-theme.js';
 
+import connectToStores from 'alt-utils/lib/connectToStores';
+
+import ChatStore from '../stores/ChatStore';
+
+@connectToStores
 class App extends React.Component {
 	constructor() {
 		super();
+	}
+
+	static getStores() {
+		return [ChatStore];
+	}
+
+	static getPropsFromStores() {
+		return ChatStore.getState();
 	}
 
 	static childContextTypes = {
@@ -26,21 +38,31 @@ class App extends React.Component {
 	}
 
 	render() {
+		var view = <Login />;
+
+		if (this.props.user) {
+			view = (
+				<div>
+					<div style={{
+						display: 'flex',
+						flexFlow: 'row wrap',
+						maxWidth: 1200,
+						width: '100%',
+						margin: '30px auto 30px'
+					}}>
+						<ChannelList />
+						<MessageList />
+					</div>
+					<MessageBox />
+					<RaisedButton label="More Info" primary={true} />
+				</div>
+			);
+		}
+
 		return (
 			<div>
 				<AppBar title="React Meets Material UI" />
-				<div style={{
-					display: 'flex',
-					flexFlow: 'row wrap',
-					maxWidth: 1200,
-					width: '100%',
-					margin: '30px auto 30px'
-				}}>
-					<ChannelList />
-					<MessageList />
-				</div>
-				<MessageBox />
-				<RaisedButton label="More Info" primary={true} />
+				{ view }
 			</div>
 		);
 	}
--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -11,31 +11,35 @@ class MessageList extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			messages: []
+			messages: {}
 		};
 
 		this.firebaseRef = new Firebase('https://react-mui.firebaseio.com/messages');
-		this.firebaseRef.on("value", (dataSnapshot) => {
-			var messagesVal = dataSnapshot.val();
-			var messages = _(messagesVal)
-				.keys()
-				.map((messageKey)=> {
-					var cloned = _.clone(messagesVal[messageKey]);
-					cloned.key = messageKey;
-					return cloned;
-				})
-				.value();
-				
-			this.setState({
-				messages: messages
-			});
+
+		this.firebaseRef.on("child_added", (msg) => {
+			if(this.state.messages[msg.key()]) {
+				return;
+			}
+
+			let msgVal = msg.val();
+			msgVal.key = msg.key();
+
+			this.state.messages[msgVal.key] = msgVal;
+			this.setState({ messages: this.state.messages });
 		});
+
+		this.firebaseRef.on("child_removed", (msg) => {
+			let key = msg.key();
+			delete this.state.messages[key];
+			this.setState({ messages: this.state.messages });
+		});
+
 	}
 
 	render() {
 		// debugger;
 
-		var messageNodes = this.state.messages.map((message) => {
+		var messageNodes = _.values(this.state.messages).map((message) => {
 			return (
 				<div>
 						<Message message={ message.message } />
